c2cca678806e1890b32938f1e8f1e31724b81277
Алексей Бельченков <u608110@gmail.com>
2024-05-04 22:28:16 +0400

    switch screens

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_updated/questions_screen.dart';
 
 import 'package:quiz_updated/start_screen.dart';
+import 'package:quiz_updated/questions_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -12,6 +14,20 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
@@ -27,7 +43,7 @@ class _QuizState extends State<Quiz> {
                 end: Alignment.bottomRight,
               )
           ),
-          child: const StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
