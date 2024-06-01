8a2a243084cfa43b2f9477e48070f0dfd501c27d
Алексей Бельченков <u608110@gmail.com>
2024-06-01 19:38:39 +0400

    Add google fonts

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'answer_button.dart';
 import 'package:quiz_updated/data/questions.dart';
@@ -34,8 +35,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Text(currentQuestion.text,
-                style: const TextStyle(
-                  color: Colors.white,
+                style: GoogleFonts.tiroTamil(
+                  color: const Color.fromARGB(255, 201, 153, 251),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
                 textAlign: TextAlign.center,
               ),
